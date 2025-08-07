1275b7b6f426f8f47a7859950697e8e2188b30d1
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2025-08-07 05:39:55 -0400

    Squashed commit of the following:
    commit 5049806a1831d409df63f8beda8353c7096fe76f
    Author: andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
    Date:   Thu Aug 7 05:39:46 2025 -0400
    
        Syncing with main
    
    commit 547369af4d042114a99b819f43715c3148831b35
    Merge: 6dd2bcd 4392d8f
    Author: andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
    Date:   Thu Aug 7 05:37:48 2025 -0400
    
        Merge branch 'main' into euclid
    
    commit 6dd2bcd5f7f49882f6285ec39c2fc047fa4f7f88
    Author: andy-stewart-design <robestewart@ebay.com>
    Date:   Wed Aug 6 16:53:10 2025 -0400
    
        Simplifying implementation
    
    commit 7fd7644f46b9d33d61a551642109dd28fc28b895
    Author: andy-stewart-design <robestewart@ebay.com>
    Date:   Wed Aug 6 15:12:16 2025 -0400
    
        Adding rotation option to euclid function
    
    commit eae716e4126141b2efd23f76ca9a1479a1e5e82a
    Author: andy-stewart-design <robestewart@ebay.com>
    Date:   Wed Aug 6 15:02:59 2025 -0400
    
        Creating euclid function

--- src/synth.ts
+++ src/synth.ts
@@ -1,5 +1,6 @@
 import { beep } from "./beep";
 import type { AudioClock } from "./clock";
+import { euclid } from "./euclid";
 
 class Synth {
   private ctx: AudioContext;
@@ -7,7 +8,7 @@ class Synth {
   private notes: number[] = [261.63];
   private noteOffsets: number | number[] = 0;
   private waveform: OscillatorType = "sine";
-  private adsr = { attack: 0.0, decay: 0.2, sustain: 0.0, release: 0.0 };
+  private adsr = { attack: 0.01, decay: 0.2, sustain: 0.0, release: 0.1 };
 
   constructor(clock: AudioClock) {
     this.ctx = clock.ctx;
@@ -15,9 +16,8 @@ class Synth {
   }
 
   public note(n: number | number[]) {
-    const numNotes = Array.isArray(n) ? n.length : 1;
     this.notes = Array.isArray(n) ? n : [n];
-    this.noteOffsets = this.duration / numNotes;
+    this.noteOffsets = this.duration / this.notes.length;
     return this;
   }
 
@@ -26,14 +26,36 @@ class Synth {
     return this;
   }
 
-  public play(time: number) {
-    console.log(this.notes);
+  public fast(multiplier: number) {
+    const newLength = Math.floor(this.notes.length * multiplier);
+    this.notes = Array.from(
+      { length: newLength },
+      (_, i) => this.notes[i % this.notes.length]
+    );
+    this.noteOffsets = this.duration / this.notes.length;
+    return this;
+  }
 
+  public euclid(pulses: number, steps: number, rotation = 0) {
+    const pattern = euclid(pulses, steps, rotation);
+    const totalNotes = steps;
+    this.noteOffsets = this.duration / totalNotes;
+
+    let noteIndex = 0;
+    this.notes = pattern.map((p) => {
+      return p === 0 ? 0 : this.notes[noteIndex++ % this.notes.length];
+    });
+
+    return this;
+  }
+
+  public play(time: number) {
     this.notes?.forEach((frequency, i) => {
+      if (frequency === 0) return; // Skip silent notes
       const { ctx, duration, waveform, noteOffsets, adsr } = this;
       const offset = Array.isArray(noteOffsets) ? noteOffsets[i] : noteOffsets;
       const t = time + offset * i;
-      const totalVoices = 1;
+      const totalVoices = 1.5;
       beep({
         ctx,
         waveform,
